eb152e9bae0f99160d3d7307b2ff6b54c118355c
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2026-06-09 15:03:54 -0400

    Initial commit

---
+++ src/main/java/edu/odu/cs/cs330/items/Armour.java
@@ -0,0 +1,142 @@
+package edu.odu.cs.cs330.items;
+
+import java.util.Scanner;
+
+/**
+ * This class represents one piece of armour--as found in most video games.
+ * This includes boots and helmets.
+ *
+ * Armour may not be stacked.
+ */
+@SuppressWarnings({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.CloneMethodReturnTypeMustMatchClassName",
+    "PMD.CloneThrowsCloneNotSupportedException",
+    "PMD.LawOfDemeter",
+    "PMD.OnlyOneReturn",
+    "PMD.ProperCloneImplementation",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LocalVariableCouldBeFinal"
+})
+public class Armour extends Equippable {
+    /**
+     * The amount of damage that can be negated.
+     */
+    protected int defense;
+
+    /**
+     * Default to a armour with an empty name, zero durability, zero defense,
+     * blank material, no modifier a zero modifier level, and a blank element.
+     */
+    public Armour()
+    {
+        super();
+
+        this.defense = 0;
+    }
+
+    /**
+     * Duplicate a piece of armour.
+     *
+     * @param src armour to duplicate
+     */
+    public Armour(Armour src)
+    {
+        // Complete this function.
+    }
+
+    /**
+     * Retrieve armour defense.
+     *
+     * @return total defense provided
+     */
+    public int getDefense()
+    {
+        return this.defense;
+    }
+
+    /**
+     * Update defense.
+     *
+     * @param def replacement defense
+     */
+    public void setDefense(int def)
+    {
+        this.defense = def;
+    }
+
+    /**
+     * Read Armour attributes.
+     */
+    @Override
+    public void read(Scanner snr)
+    {
+        super.name    = snr.next();
+
+        // Complete this function.
+    }
+
+    /**
+     * Clone--i.e., copy--this Armour.
+     */
+    @Override
+    public Item clone()
+    {
+        Armour cpy = new Armour();
+
+        // Complete this function.
+
+        return cpy;
+    }
+
+    /**
+     * Check for logical equivalence--based on name, material, modifier, and
+     * element.
+     *
+     * @param rhs object for which a comparison is desired
+     */
+    @Override
+    public boolean equals(Object rhs)
+    {
+        if (!(rhs instanceof Armour)) {
+            return false;
+        }
+
+        Armour rhsItem = (Armour) rhs;
+
+        // Complete this function.
+        // Remove the placeholder return
+        return false;
+    }
+
+    /**
+     * Generate a hash code by adding the name, material, modifier, and element
+     * hash codes.
+     */
+    @Override
+    public int hashCode()
+    {
+        // Complete this function.
+        // Remove the placeholder return
+        return -1;
+    }
+
+    /**
+     * *Print* one Armour.
+     */
+    @Override
+    public String toString()
+    {
+
+        // Complete this function... treat the return as a hint.
+        return String.join(
+            System.lineSeparator(),
+            String.format("  Nme: %s", super.getName()),
+            ""
+        );
+    }
+}
+
+
+
+
---
+++ src/main/java/edu/odu/cs/cs330/items/Consumable.java
@@ -0,0 +1,165 @@
+package edu.odu.cs.cs330.items;
+
+import java.util.Scanner;
+
+/**
+ * This class represents one Consumable Item--as found in most video games.
+ * This includes food.
+ *
+ * Consumable Items must be stackable.
+ */
+@SuppressWarnings({
+    "PMD.BeanMembersShouldSerialize",
+    "PMD.CloneMethodReturnTypeMustMatchClassName",
+    "PMD.CloneThrowsCloneNotSupportedException",
+    "PMD.LawOfDemeter",
+    "PMD.OnlyOneReturn",
+    "PMD.ProperCloneImplementation",
+    "PMD.MethodArgumentCouldBeFinal",
+    "PMD.LocalVariableCouldBeFinal",
+    "PMD.BeanMembersShouldSerialize"
+})
+public class Consumable extends Item {
+    /**
+     * The effect "buff" or "debuff" that is received when using this item.
+     */
+    protected String effect;
+
+    /**
+     * The number of times this item can be used.
+     */
+    protected int uses;
+
+    /**
+     * Default to a Consumable Item with an empty name, no effect and zero
+     * uses.
+     */
+    public Consumable()
+    {
+        super("[Placeholder]", true);
+
+        this.effect = "";
+        this.uses   = 0;
+    }
+
+    /**
+     * Create a copy of this Consumable.
+     *
+     * @param src consumable item to duplicate
+     */
+    public Consumable(Consumable src)
+    {
+        // Complete this function.
+        // Update/replace the call to super
+        super("[Placeholder]", true);
+    }
+
+    /**
+     * Retrieve the effect.
+     *
+     * @return the set effect (i.e., buff or debuff)
+     */
+    public String getEffect()
+    {
+        return this.effect;
+    }
+
+    /**
+     * Set a new buff or debuff.
+     *
+     * @param newEff replacement effect
+     */
+    public void setEffect(String newEff)
+    {
+        this.effect = newEff;
+    }
+
+    /**
+     * Retrieve permitted number of uses.
+     *
+     * @return number of total uses
+     */
+    public int getNumberOfUses()
+    {
+        return this.uses;
+    }
+
+    /**
+     * Set the number of permitted uses.
+     *
+     * @param allowed number of allowed uses
+     */
+    public void setNumberOfUses(int allowed)
+    {
+        this.uses = allowed;
+    }
+
+    /**
+     * Read Consumable Item attributes.
+     */
+    @Override
+    public void read(Scanner snr)
+    {
+        super.name    = snr.next();
+
+        // Complete this function.
+    }
+
+    /**
+     * Clone--i.e., copy--this Consumable Item.
+     */
+    @Override
+    public Item clone()
+    {
+        Consumable cpy = new Consumable();
+
+        // Complete this function.
+
+        return cpy;
+    }
+
+    /**
+     * Check for logical equivalence--based on name and effect.
+     *
+     * @param rhs object for which a comparison is desired
+     */
+    @Override
+    public boolean equals(Object rhs)
+    {
+        if (!(rhs instanceof Consumable)) {
+            return false;
+        }
+
+        Consumable rhsItem = (Consumable) rhs;
+
+        // Use the provided return as a start/hint
+        return this.name.equals(rhsItem.name);
+    }
+
+    /**
+     * Generate a hash code based on name and effect.
+     *
+     * Add <code>name.hashCode()</code> and <code>effect.hashCode</code>, then
+     * return the result.
+     */
+    @Override
+    public int hashCode()
+    {
+        // Use the provided return as a start/hint
+        return this.name.hashCode();
+    }
+
+    /**
+     * *Print* the Consumable Item.
+     */
+    @Override
+    public String toString()
+    {
+        // Complete this function... treat the return as a hint.
+        return String.join(
+            System.lineSeparator(),
+            String.format("  Nme: %s", super.getName()),
+            ""
+        );
+    }
+}
